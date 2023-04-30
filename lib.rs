823a8b88bc6074c0362abb94c270c99372d68a10
Shankar Rao Mata <mshankarrao1970@gmail.com>
2023-05-01 03:49:20 +0530

    added the unit tests for the functions

--- lib.rs
+++ lib.rs
@@ -41,4 +41,40 @@ mod counter {
 
     }
 
+
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let counter = Counter::default();
+            assert_eq!(counter.get_counter(), 0);
+        }
+
+        /// We test a simple use case of our contract.
+        #[ink::test]
+        fn increment_works() {
+            let mut counter = Counter::new(1);
+            assert_eq!(counter.get_counter(), 1);
+            counter.increment_counter();
+            assert_eq!(counter.get_counter(), 2);
+        }
+
+        /// We test a simple use case of our contract.
+        #[ink::test]
+        fn decrement_works() {
+            let mut counter = Counter::new(1);
+            assert_eq!(counter.get_counter(), 1);
+            counter.decrement_counter();
+            assert_eq!(counter.get_counter(), 0);
+        }
+    }
+
 }
